dea5264099388767f1d01bcb02b6f357a5258aba
Dominic Padula <domopadula2192@gmail.com>
2020-12-01 08:49:27 -0700

    tests and methods for iteration 1 up to remove card

---
+++ lib/deck.rb
@@ -0,0 +1,24 @@
+class Deck
+  attr_reader:cards
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def rank_of_card_at(index)
+    @cards.fetch(index).rank
+  end
+
+  def high_ranking_cards
+    @cards.select do |card|
+      card.rank >= 11
+    end
+  end
+
+  def percent_high_ranking
+    ((high_ranking_cards.count / @cards.count.to_f) * 100).round(2)
+  end
+
+  def remove_card
+    @cards.shift 
+  end
+end

c712a151a4598a66354c42fd7afc4b324ec8efe2
Dominic Padula <domopadula2192@gmail.com>
2020-12-01 09:00:02 -0700

    add code that pushes cards into cards array

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -8,6 +8,7 @@ class DeckTest < Minitest::Test
       @card1 = Card.new(:diamond, 'Queen', 12)
       @card2 = Card.new(:spade, '3', 3)
       @card3 = Card.new(:heart, 'Ace', 14)
+      @card4 = Card.new(:club, '5', 5)
       @cards = [@card1, @card2, @card3]
       @deck = Deck.new(@cards)
   end
@@ -31,6 +32,18 @@ class DeckTest < Minitest::Test
 
   def test_remove_card
     assert_equal @card1, @deck.remove_card
-    assert_equal [@card2,@card3], @deck.cards 
+    assert_equal [@card2,@card3], @deck.cards
+  end
+
+  def test_remove_card_changes_high_ranking_methods
+    @deck.remove_card
+    assert_equal [@card3], @deck.high_ranking_cards
+    assert_equal 50.0, @deck.percent_high_ranking
+
+  end
+
+  def test_we_can_add_cards
+    @deck.add_card(@card4)
+    assert_equal [@card1,@card2,@card3,@card4], @deck.cards 
   end
 end

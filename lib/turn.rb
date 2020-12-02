4026c3d0ebb51722b6bc4512ebf6d89ff2101cd1
Dominic Padula <domopadula2192@gmail.com>
2020-12-02 06:27:38 -0700

    Refactor code to pass tests for type and winner

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,14 +14,24 @@ class Turn
       :basic
     elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
       :war
-    end 
+    end
   end
 
   def winner
+    if type == :basic
+
     if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
       @player1
     else
       @player2
+    end
+
+  elsif type == :war
+    if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+      @player1
+    else
+      @player2
+      end
     end
   end
 

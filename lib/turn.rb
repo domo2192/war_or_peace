e3075047ce9a3a93fedc42a7623c31aed1ce11aa
Dominic Padula <domopadula2192@gmail.com>
2020-12-01 20:11:35 -0700

    add passing code for award spoils pile cards and winner

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,41 @@
 class Turn
   attr_reader:player1,
-             :player2
+             :player2,
+             :spoils_of_war
+
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def type
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      :basic
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      :war
+    end 
+  end
+
+  def winner
+    if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+      @player1
+    else
+      @player2
+    end
+  end
+
+  def pile_cards
+    @spoils_of_war << @player1.deck.cards.first
+    @spoils_of_war << @player2.deck.cards.first
+    @player1.deck.cards.shift
+    @player2.deck.cards.shift
+  end
+
+  def award_spoils(winner)
+    @spoils_of_war.each do |spoil|
+      winner.deck.cards << spoil
+    end
+    @spoils_of_war = []
   end
 end

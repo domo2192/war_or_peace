653aa108bdc711283ada2a9daa30810dded54a57
Dominic Padula <domopadula2192@gmail.com>
2020-12-02 17:46:38 -0700

    setup game class tests and add file

---
+++ test/game_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+
+class GameTest < Minitest::Test
+
+end

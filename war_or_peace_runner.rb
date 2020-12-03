a708d9df7b56e5045cfeb2f5acd3e92630cace37
Dominic Padula <domopadula2192@gmail.com>
2020-12-02 17:46:09 -0700

    Add two full decks and split them

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -3,3 +3,22 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 require './lib/game'
+require'pry'
+
+
+cards = []
+suits = [:Diamond, :Heart, :Spade, :Club]
+values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+rank_of_cards = {"2"=> 2, "3"=> 3, "4"=> 4, "5"=> 5, "6"=>6 , "7"=>7, "8"=>8, "9"=>9, "10"=>10, "Jack"=>11, "Queen"=>12, "King"=>13, "Ace"=>14}
+
+suits.each do |suit|
+  values.each do |value|
+    rank = rank_of_cards[value]
+    cards << Card.new(suit,value,rank)
+  end
+end
+deck1 = Deck.new(cards.shuffle[0..25])
+deck2 = Deck.new(cards.shuffle[26..51])
+
+p deck1.sort
+p deck2.sort
